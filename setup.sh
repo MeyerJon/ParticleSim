b8237e3e4c5a69a0dcab26735c57e19385943668
Jonathan Meyer <Jonathan.Meyer@student.uantwerpen.be>
2019-01-29 15:53:50 +0100

    Fixed setup.sh
    Following name-change of requirements file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 if [[ "$(virtualenv --version)" =~ (16[.]) ]]; then
 	virtualenv ./venv/
 	source ./venv/bin/activate
-	pip3 install -r req.txt
+	pip3 install -r requirements.txt
 else
 	echo "Please use virtualenv version 16."
 fi
